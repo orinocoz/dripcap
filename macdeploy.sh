5ce09ca2ddfc4d5442a52578b6ac13d50bbbfd12
h2so5 <mail@h2so5.net>
2016-02-01 23:17:46 +0900

    fix paperfilter archive name on macdeploy.sh

--- macdeploy.sh
+++ macdeploy.sh
@@ -5,7 +5,7 @@ cd .builtapp
 
 git clone https://github.com/dripcap/dripcap-helper.git
 cp ./dripcap-darwin/dripcap.app/Contents/Resources/app/node_modules/paperfilter/bin/paperfilter dripcap-helper/paperfilter
-zip -j ../paperfilter-darwin.zip ./dripcap-darwin/dripcap.app/Contents/Resources/app/node_modules/paperfilter/bin/paperfilter
+zip -j ../paperfilter-darwin-amd64.zip ./dripcap-darwin/dripcap.app/Contents/Resources/app/node_modules/paperfilter/bin/paperfilter
 plutil -replace 'CFBundleVersion' -string `node -p "require('../package.json').version"` dripcap-helper/DripcapHelper/Info.plist
 cd dripcap-helper
 xcodebuild -configuration Release
