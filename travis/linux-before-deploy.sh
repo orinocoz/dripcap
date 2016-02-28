bf40308bbd8b203a64ab25b18eaaf001e2b20cba
h2so5 <mail@h2so5.net>
2016-02-28 12:24:48 +0900

    rpm: keep package version

--- travis/linux-before-deploy.sh
+++ travis/linux-before-deploy.sh
@@ -1,4 +1,4 @@
 npm run dpkg
-fakeroot alien --to-rpm --scripts dripcap-linux-amd64.deb
+fakeroot alien --to-rpm -k --scripts dripcap-linux-amd64.deb
 mv *.rpm dripcap-linux-amd64.rpm
 zip -j paperfilter-linux-amd64.zip .build/node_modules/paperfilter/bin/paperfilter
