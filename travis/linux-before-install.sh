d247df84654e10abdd290144f109e3e4b19cfc16
h2so5 <mail@h2so5.net>
2016-09-03 15:52:06 +0900

    travis: download libpcap-1.7.4.tar.gz from socket.moe

--- travis/linux-before-install.sh
+++ travis/linux-before-install.sh
@@ -5,7 +5,7 @@ nvm install $NODE_VERSION
 nvm use --delete-prefix $NODE_VERSION
 
 export CXX="g++-4.9"
-wget http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz
+wget https://socket.moe/storage/libpcap-1.7.4.tar.gz
 tar xzf libpcap-1.7.4.tar.gz
 (cd libpcap-1.7.4 && ./configure -q --enable-shared=no && make -j2 && sudo make install)
 
