e783ed8406fc24f0e4b1b46c86891dcbc356f8ce
h2so5 <mail@h2so5.net>
2016-09-16 23:16:24 +0900

    goldfilter: update linux build

--- travis/linux-before-install.sh
+++ travis/linux-before-install.sh
@@ -4,20 +4,20 @@ source ~/.nvm/nvm.sh
 nvm install $NODE_VERSION
 nvm use --delete-prefix $NODE_VERSION
 
-export CXX="g++-4.9"
+export CC="gcc-5"
+export CXX="g++-5"
 wget https://socket.moe/storage/libpcap-1.7.4.tar.gz
 tar xzf libpcap-1.7.4.tar.gz
 (cd libpcap-1.7.4 && ./configure -q --enable-shared=no && make -j2 && sudo make install)
-
-git clone --depth=1 https://github.com/facebook/rocksdb.git
-(cd rocksdb && PORTABLE=1 make static_lib -j2 && sudo make install)
-
-export DISPLAY=':99.0'
 
 sudo apt-get remove libicu-dev
 wget `curl https://api.github.com/repos/dripcap/libv8/releases | jq -r '(.[0].assets[] | select(.name == "v8-linux-amd64.deb")).browser_download_url'`
 sudo dpkg -i --force-overwrite v8-linux-amd64.deb
 
+wget `curl https://api.github.com/repos/dripcap/librocksdb/releases | jq -r '(.[0].assets[] | select(.name == "rocksdb-linux-amd64.deb")).browser_download_url'`
+sudo dpkg -i rocksdb-linux-amd64.deb
+
 export GOPATH=/home/travis/gopath
 export GOBIN=/home/travis/gopath/bin
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+export DISPLAY=':99.0'
