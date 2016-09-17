b637cec1ec2799f7bb9898065b69f2b0dbc25120
Ron Hashimoto <mail@h2so5.net>
2016-09-18 01:25:26 +0900

    Update windows-before-install.ps1

--- travis/windows-before-install.ps1
+++ travis/windows-before-install.ps1
@@ -2,6 +2,8 @@ choco install jq curl
 
 curl.exe -s -S -k https://api.github.com/repos/dripcap/libv8/releases | jq -r '(.[0].assets[] | select(.name == \"v8-windows-amd64.deb\")).browser_download_url'
 curl.exe -s -S -k https://api.github.com/repos/dripcap/librocksdb/releases | jq -r '(.[0].assets[] | select(.name == \"rocksdb-windows-amd64.deb\")).browser_download_url'
+Expand-Archive -Path .\v8-windows-amd64.zip -DestinationPath $env:HOMEPATH -Force
+Expand-Archive -Path .\rocksdb-windows-amd64.zip -DestinationPath $env:HOMEPATH -Force
 
 npm config set loglevel error
 npm install -g gulp electron babel-cli
