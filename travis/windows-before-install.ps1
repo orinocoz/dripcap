f81181b93855afa1971373273ae562dd66655a75
Ron Hashimoto <mail@h2so5.net>
2016-09-18 02:46:22 +0900

    Update windows-before-install.ps1

--- travis/windows-before-install.ps1
+++ travis/windows-before-install.ps1
@@ -8,6 +8,7 @@ curl.exe -s -S -k -L -O $url
 Expand-Archive -Path .\v8-windows-amd64.zip -DestinationPath $env:HOMEPATH -Force
 Expand-Archive -Path .\rocksdb-windows-amd64.zip -DestinationPath $env:HOMEPATH -Force
 
+Install-Product node ''
 npm config set loglevel error
 npm install -g gulp electron babel-cli
 npm install babel-plugin-add-module-exports babel-plugin-transform-async-to-generator babel-plugin-transform-es2015-modules-commonjs
