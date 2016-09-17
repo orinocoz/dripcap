05800c7856d237699138e20bd62b1ff53025f65a
Ron Hashimoto <mail@h2so5.net>
2016-09-18 01:30:36 +0900

    Update windows-before-install.ps1

--- travis/windows-before-install.ps1
+++ travis/windows-before-install.ps1
@@ -1,7 +1,10 @@
 choco install jq curl
 
-curl.exe -s -S -k https://api.github.com/repos/dripcap/libv8/releases | jq -r '(.[0].assets[] | select(.name == \"v8-windows-amd64.deb\")).browser_download_url'
-curl.exe -s -S -k https://api.github.com/repos/dripcap/librocksdb/releases | jq -r '(.[0].assets[] | select(.name == \"rocksdb-windows-amd64.deb\")).browser_download_url'
+$url = curl.exe -s -S -k https://api.github.com/repos/dripcap/libv8/releases | jq -r '(.[0].assets[] | select(.name == \"v8-windows-amd64.zip\")).browser_download_url'
+curl.exe -k -L -O $url
+$url = curl.exe -s -S -k https://api.github.com/repos/dripcap/librocksdb/releases | jq -r '(.[0].assets[] | select(.name == \"rocksdb-windows-amd64.zip\")).browser_download_url'
+curl.exe -k -L -O $url
+
 Expand-Archive -Path .\v8-windows-amd64.zip -DestinationPath $env:HOMEPATH -Force
 Expand-Archive -Path .\rocksdb-windows-amd64.zip -DestinationPath $env:HOMEPATH -Force
 
