df78ca9cc1747027f228680a5bdab044099fb2bb
Ron Hashimoto <mail@h2so5.net>
2016-09-17 22:52:47 +0900

    Create windows-before-install.ps1

---
+++ travis/windows-before-install.ps1
@@ -0,0 +1,6 @@
+curl.exe -k https://api.github.com/repos/dripcap/libv8/releases | jq -r '(.[0].assets[] | select(.name == \"v8-windows-amd64.deb\")).browser_download_url'
+curl.exe -k https://api.github.com/repos/dripcap/librocksdb/releases | jq -r '(.[0].assets[] | select(.name == \"rocksdb-windows-amd64.deb\")).browser_download_url'
+choco install jq
+npm install -g gulp electron babel-cli
+npm install babel-plugin-add-module-exports babel-plugin-transform-async-to-generator babel-plugin-transform-es2015-modules-commonjs
+npm install
