2a6976ef26c81e6fe55890a89a8adbbe2e33331f
Ron Hashimoto <mail@h2so5.net>
2016-09-18 00:19:46 +0900

    clp

--- goldfilter/build.js
+++ goldfilter/build.js
@@ -2,7 +2,7 @@ const childProcess = require('child_process');
 
 var proc;
 if (process.platform === 'win32') {
-  proc = childProcess.exec('MSBuild goldfilter.sln /t:Rebuild /p:Configuration=Release /m');
+  proc = childProcess.exec('MSBuild goldfilter.sln /clp:ErrorsOnly /t:Rebuild /p:Configuration=Release /m');
 } else {
   proc = childProcess.exec('make -j2 NODEBUG=true');
 }
