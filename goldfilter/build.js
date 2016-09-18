62cdcce72e96665f7c9bbdbd89193ad6d7907719
Ron Hashimoto <mail@h2so5.net>
2016-09-18 15:16:21 +0900

    goldfilter: fix build.js

--- goldfilter/build.js
+++ goldfilter/build.js
@@ -5,7 +5,7 @@ if (process.platform === 'win32') {
   if (process.env['NOWINPCAP'] === '1') {
       proc = childProcess.exec('MSBuild goldfilter.sln /p:ForceImportBeforeCppTargets=' + __dirname + '\\nowinpcap.props /clp:ErrorsOnly /t:Rebuild /p:Configuration=Release /m');
   } else {
-      proc = childProcess.exec('MSBuild goldfilter.sln /clp:ErrorsOnly /t:Rebuild /p:Configuration=Release /m');
+      proc = childProcess.exec('MSBuild goldfilter.sln /clp:ErrorsOnly /t:Build /p:Configuration=Release /m');
   }
 } else {
   proc = childProcess.exec('make -j2 NODEBUG=true');
